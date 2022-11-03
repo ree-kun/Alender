0372767d4139950674fa738e493ce15644910c1b
RyoIto <ryo.ito0729@gmail.com>
2022-11-03 18:07:00 +0900

    プリサウンドを鳴らしてからTTSを行うようにした。

--- app/src/main/java/com/example/customalarm/NotifyActivity.kt
+++ app/src/main/java/com/example/customalarm/NotifyActivity.kt
@@ -1,54 +1,114 @@
 package com.example.customalarm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
 import android.util.Log
-import android.view.View
+import android.view.WindowManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.customalarm.data.db.AlarmSettingDao
 import com.example.customalarm.data.db.AppDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class NotifyActivity : AppCompatActivity(), View.OnClickListener, TextToSpeech.OnInitListener {
+class NotifyActivity : AppCompatActivity(), MediaPlayer.OnCompletionListener, TextToSpeech.OnInitListener {
 
     private val scope = CoroutineScope(Dispatchers.Default)
 
     private lateinit var alarmSettingDao: AlarmSettingDao
+    private var mediaPlayer: MediaPlayer? = null
     private lateinit var tts: TextToSpeech
 
+    private lateinit var stopBtn: Button
     private lateinit var speechText: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // TODO 適切な設定を確認する。
+        window.addFlags(
+            (WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or
+                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        )
+
         setContentView(R.layout.activity_notify)
 
         alarmSettingDao = AppDatabase.getDatabase(applicationContext).alarmSettingDao()
         tts = TextToSpeech(this, this)
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound)
+        mediaPlayer!!.setOnCompletionListener(this)
+
         val alarmId = intent.getIntExtra("alarmId", -1)
-
-//        val ttsButton = findViewById<Button>(R.id.stopBtn)
-//        ttsButton.setOnClickListener(this)
-
         scope.launch {
             val alarmSettingEntity = alarmSettingDao.selectById(alarmId)
             speechText = alarmSettingEntity.title
         }
+
+        play()
+
+        stopBtn = findViewById(R.id.stopBtn)
+        stopBtn.setOnClickListener {
+            tts.stop()
+            tts.shutdown()
+            finish()
+        }
     }
 
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+    override fun onDestroy() {
+        super.onDestroy()
+        stop()
+    }
+
+    private fun play() {
+        mediaPlayer!!.start()
+    }
+
+    private fun stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer!!.stop()
+            mediaPlayer!!.reset()
+            mediaPlayer!!.release()
+            mediaPlayer = null
+        }
+    }
+
+    override fun onCompletion(mediaPlayer: MediaPlayer?) {
+        stop()
+        tts.speak(speechText, TextToSpeech.QUEUE_FLUSH, null, "notifyMessage")
     }
 
     override fun onInit(status: Int) {
         if (TextToSpeech.SUCCESS == status) {
-            Log.d("debug", "initialized")
-            tts.speak(speechText, TextToSpeech.QUEUE_FLUSH, null, "notifyMessage")
-            finish()
+            println("debug: initialized")
+            setTtsListener()
         } else {
-            Log.d("debug", "failed to initialized")
+            println("debug: failed to initialized")
+        }
+    }
+
+    private fun setTtsListener() {
+        val listenerResult = tts.setOnUtteranceProgressListener(object: UtteranceProgressListener() {
+            override fun onDone(utteranceId: String) {
+                Log.d("debug", "progress on Done $utteranceId")
+                this@NotifyActivity.finish()
+            }
+
+            override fun onError(utteranceId: String) {
+                Log.d("debug", "progress on Error $utteranceId")
+            }
+
+            override fun onStart(utteranceId: String) {
+                Log.d("debug", "progress on Start $utteranceId")
+            }
+        })
+
+        if (listenerResult != TextToSpeech.SUCCESS) {
+            Log.e("debug", "faild to add utterance progress listener")
         }
     }
 
